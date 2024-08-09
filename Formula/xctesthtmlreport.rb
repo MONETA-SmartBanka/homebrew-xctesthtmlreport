088d7dca59e1f93263075dbdecce64c6a0c24904
Tomas Srna <tomas@srna.net>
2024-08-09 09:30:00 +0200

    Initial

---
+++ Formula/xctesthtmlreport.rb
@@ -0,0 +1,18 @@
+class Xctesthtmlreport < Formula
+  desc "Xcode-like HTML report for Unit and UI Tests [MONETA]"
+  homepage "https://github.com/MONETA-SmartBanka/XCTestHTMLReport"
+  url "https://github.com/MONETA-SmartBanka/XCTestHTMLReport/archive/refs/tags/2.2.5.tar.gz"
+  sha256 "3c7f9f85ae550b9f3138734a44db068fcd42b720f6e18370af3e767d02363019"
+  license "MIT"
+  head "https://github.com/MONETA-SmartBanka/XCTestHTMLReport.git", branch: "main"
+
+  depends_on :macos
+  depends_on xcode: "14.0"
+  uses_from_macos "swift"
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/xchtmlreport"
+  end
+end
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# Moneta-smartbanka Xctesthtmlreport
+
+## How do I install these formulae?
+
+`brew install moneta-smartbanka/xctesthtmlreport/<formula>`
+
+Or `brew tap moneta-smartbanka/xctesthtmlreport` and then `brew install <formula>`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "moneta-smartbanka/xctesthtmlreport"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
