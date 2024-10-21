cb702c282a6bf02a9d09d4e37340405771312a86
Matěj Děcký <matejdecky@gmail.com>
2024-10-21 12:26:24 +0200

    Update version

--- Formula/xctesthtmlreport.rb
+++ Formula/xctesthtmlreport.rb
@@ -1,13 +1,13 @@
 class Xctesthtmlreport < Formula
   desc "Xcode-like HTML report for Unit and UI Tests [MONETA]"
   homepage "https://github.com/MONETA-SmartBanka/XCTestHTMLReport"
-  url "https://github.com/MONETA-SmartBanka/XCTestHTMLReport/archive/refs/tags/2.2.5.tar.gz"
-  sha256 "3c7f9f85ae550b9f3138734a44db068fcd42b720f6e18370af3e767d02363019"
+  url "https://github.com/MONETA-SmartBanka/XCTestHTMLReport/archive/refs/tags/2.5.0.tar.gz"
+  sha256 "6249242b4fd6e008b450839b2cc053c3c0646a2650480e74238b5230db0a657c"
   license "MIT"
   head "https://github.com/MONETA-SmartBanka/XCTestHTMLReport.git", branch: "main"
 
   depends_on :macos
-  depends_on xcode: "14.0"
+  depends_on xcode: "16.0"
   uses_from_macos "swift"
 
   def install
